688562cd6165250d3ad64b2096d1f4d7ac272c10
Jose Arjona <arjonatorres79@gmail.com>
2018-01-25 12:31:05 +0100

    Añadida tabla al gestionar alquileres

--- views/alquileres/gestionar.php
+++ views/alquileres/gestionar.php
@@ -19,12 +19,28 @@ use yii\widgets\ActiveForm;
         <?php ActiveForm::end() ?>
     </div>
     <div class="col-md-6">
-        <?php if (isset($socio)): ?>
-            <h3><?= Html::encode($socio->nombre) ?></h3>
-            <h3><?= Html::encode($socio->telefono) ?></h3>
-            <?php foreach ($socio->pendientes as $alquiler): ?>
-                
-            <?php endforeach ?>
-        <?php endif ?>
+        <h3><?= Html::encode($socio->nombre) ?></h3>
+        <h3><?= Html::encode($socio->telefono) ?></h3>
     </div>
+</div>
+
+<div class="row">
+    <table class="table">
+        <thead>
+            <th>Número</th>
+            <th>Nombre</th>
+            <th>Fecha de alquiler</th>
+        </thead>
+        <tbody>
+            <?php if (isset($socio)): ?>
+                <?php foreach ($socio->getPendientes()->with('pelicula')->all() as $alquiler): ?>
+                    <tr>
+                        <td><?= Html::encode($alquiler->pelicula->codigo) ?></td>
+                        <td><?= Html::encode($alquiler->pelicula->titulo) ?></td>
+                        <td><?= Html::encode($alquiler->created_at) ?></td>
+                    </tr>
+                <?php endforeach ?>
+            <?php endif ?>
+        </tbody>
+    </table>
 </div>
